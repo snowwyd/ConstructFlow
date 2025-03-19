f1732a83e92e37afcc6882d6abde61f537754592
snowwyd <snowwydaniel@gmail.com>
2025-03-20 02:44:17 +0300

    #cf Fixed JWT HTTP-Only cookies

--- backend/internal/infrastructure/postgresrepo/postgres.go
+++ backend/internal/infrastructure/postgresrepo/postgres.go
@@ -1,7 +1,6 @@
 package postgresrepo
 
 import (
-	"backend/internal/domain"
 	"backend/pkg/config"
 	"fmt"
 
@@ -26,46 +25,5 @@ func New(cfg *config.Config) (*Database, error) {
 		return nil, fmt.Errorf("%s: %w", op, err)
 	}
 
-	// if err := DropAllTables(db); err != nil {
-	// 	return nil, fmt.Errorf("%s: failed to drop tables: %w", op, err)
-	// }
-
-	// Применение миграций через golang-migrate
-	// if err := applyMigrations(db); err != nil {
-	// 	return nil, fmt.Errorf("%s: %w", op, err)
-	// }
-
 	return &Database{db: db}, nil
-}
-
-// applyMigrations выполняет автоматические миграции для всех моделей
-func applyMigrations(db *gorm.DB) error {
-	return db.AutoMigrate(
-		&domain.User{},
-		&domain.Role{},
-		&domain.Directory{},
-		&domain.File{},
-		&domain.UserDirectory{},
-		&domain.UserFile{},
-		&domain.Approval{},
-		&domain.Annotation{},
-		&domain.Workflow{},
-	)
-}
-
-// TODO: разобраться с миграциями и инициализацией БД
-// DropAllTables очищает базу данных
-func DropAllTables(db *gorm.DB) error {
-	tables := []string{"users", "roles", "folders", "files", "directories", "approvals", "annotations", "workflows"}
-	for _, table := range tables {
-		if err := db.Exec(fmt.Sprintf("DROP TABLE IF EXISTS %s CASCADE", table)).Error; err != nil {
-			return fmt.Errorf("failed to drop table %s: %w", table, err)
-		}
-	}
-	return nil
-}
-
-// GetDB возвращает экземпляр GORM DB
-func (d *Database) GetDB() *gorm.DB {
-	return d.db
 }
