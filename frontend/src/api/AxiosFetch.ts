59c38ccdcdeeb4da6d0865849e60ea8d077fce0f
Глеб <gagula2011@yandex.ru>
2025-03-28 16:49:21 +0300

    #cf Added redirection after 401 response status. Ref #78

--- frontend/src/api/AxiosFetch.ts
+++ frontend/src/api/AxiosFetch.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import config from '../constants/Configurations.json';
+import { useNavigate } from 'react-router-dom';
 
 const serverURL = config.serverUrl;
 
@@ -7,5 +8,15 @@ const axiosFetching = axios.create({
 	baseURL: serverURL,
 	withCredentials: true,
 });
+
+axiosFetching.interceptors.response.use(
+	(response) => response,
+	(error) => {
+	  const navigate = useNavigate(); 
+	  if (error.response && error.response.status === 401) {
+		navigate('/'); 
+	  }
+	}
+  );
 
 export default axiosFetching;
